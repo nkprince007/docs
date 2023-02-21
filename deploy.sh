0cff4b1131e514ac94ea71d92e37fbba76c405c7
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2023-02-21 13:10:16 +0530

    static/resume.pdf: Updating resume static file

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,10 @@ bash build.sh
 echo "Deploying resume"
 vercel deploy static --prod
 vercel deploy resume-redirect --prod
+
+echo "Committing changes to git"
+git add .
+git commit -m "static/resume.pdf: Updating resume static file"
+
+echo "Pushing changes to remote origin"
+git push origin master
