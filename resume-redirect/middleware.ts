03e0a1e9490dbf6ec5c0b4088717cbe133674f52
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2023-09-10 01:14:45 +0530

    Use edge config

--- resume-redirect/middleware.ts
+++ resume-redirect/middleware.ts
@@ -1,3 +1,9 @@
-export default function middleware(request: Request) {
-    return Response.redirect(new URL('https://docs.naveenkumarsangi.me/resume.pdf'));
+import { get } from '@vercel/edge-config';
+
+export const config = {
+    runtime: 'edge',
+};
+
+export default async function middleware(request: Request) {
+    return Response.redirect(new URL(await get('targetURL') || request.url));
 }
