461b572cf689e4904770a92c81bc9f4fe8ee4120
Naveen Kumar Sangi <naveen.sangi@colortokens.com>
2024-04-20 22:40:17 +0000

    revamped resume

--- flake.nix
+++ flake.nix
@@ -4,64 +4,64 @@
   inputs =
     {
       nixpkgs.url = github:nixos/nixpkgs/nixos-22.11;
+      flake-utils.url = "github:numtide/flake-utils";
     };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      buildInputs = with pkgs; [
-        (texlive.combine {
-          inherit (texlive)
-            scheme-small
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        buildInputs = with pkgs; [
+          (texlive.combine {
+            inherit (texlive)
+              scheme-small
 
-            # Add other LaTeX libraries (packages) here as needed, e.g:
-            sourcesanspro
-            moresize
-            anyfontsize
-            csquotes
-            enumitem
-            titlesec
-            standalone
-            currfile
-            gincltex
-            svn-prov
-            adjustbox
-            collectbox
-            filemod
-            blindtext
-            ly1
+              # Add other LaTeX libraries (packages) here as needed, e.g:
+              sourcesanspro
+              moresize
+              anyfontsize
+              csquotes
+              enumitem
+              titlesec
+              standalone
+              currfile
+              gincltex
+              svn-prov
+              adjustbox
+              collectbox
+              filemod
+              blindtext
+              ly1
 
-            # build tools
-            latexmk
+              # build tools
+              latexmk
 
-            # dev tools
-            chktex
-            latexindent
-            ;
-        })
-        glibcLocales
-      ];
-    in
-    with pkgs;
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        shellHook = "echo Welcome to your nix flake powered shell environment with all your latex deps";
-        packages = buildInputs ++ [ ghostscript ];
-      };
-
-      packages.${system}.default = stdenv.mkDerivation {
-        inherit buildInputs;
-
-        name = "resume";
-
-        src = ./.;
-
-        meta = with lib; {
-          description = "My resume";
-          license = licenses.gpl3;
-          platforms = platforms.linux;
+              # dev tools
+              chktex
+              latexindent
+              ;
+          })
+          glibcLocales
+          nixpkgs-fmt
+        ];
+      in
+      with pkgs;
+      {
+        devShells.default = pkgs.mkShell {
+          shellHook = "echo Welcome to your nix flake powered shell environment with all your latex deps";
+          packages = buildInputs ++ [ ghostscript ];
         };
-      };
-    };
+
+        packages.default = stdenv.mkDerivation {
+          inherit buildInputs;
+          name = "resume";
+          src = ./.;
+          meta = with lib; {
+            description = "My resume";
+            license = licenses.gpl3;
+            platforms = platforms.linux;
+          };
+        };
+      }
+    );
 }
